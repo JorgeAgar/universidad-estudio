8a8485bbc25e396b2948e0edabebb8629a450680
Jorge <ogeandes@hotmail.com>
2023-05-26 09:01:38 -0500

    añadido ParqueCafeParcial.java desde laptop

---
+++ FundamentosDeProgramacion/src/main/java/com/universidad/fundamentosdeprogramacion/ParqueCafeParcial.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.universidad.fundamentosdeprogramacion;
+
+/**
+ *
+ * @author jorge
+ */
+import java.util.Scanner;
+public class ParqueCafeParcial {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int tickets=0, restricciones=0;
+        
+        System.out.println("Ingrese el número de atracciones mecánicas disponibles para su uso");
+        int atracciones = sc.nextInt();
+        System.out.println("Ingrese la cantidad de visitantes que se espera atender en el día");
+        int visitantes = sc.nextInt();
+        sc.nextLine();
+        
+        for(int i = 0; i < atracciones; i++){ //datos para las atracciones
+            System.out.println("==========================================");
+            System.out.println("Atracción " + (i+1));
+            System.out.println("==========================================");
+            System.out.println("Ingrese el nombre de la atracción");
+            String nombreAtraccion = sc.nextLine();
+            
+            int acrofobiaAtraccion;
+            do{
+                System.out.println("Ingrese el nivel de acrofobia de la atracción:");
+                System.out.println("1.Bajo 2.Medio 3.Alto");
+                acrofobiaAtraccion = sc.nextInt();
+            } while(acrofobiaAtraccion < 1 || acrofobiaAtraccion > 3);
+            System.out.println("Ingrese la altura mínima permitida en la atracción en cm");
+            int alturaMinAtraccion = sc.nextInt();
+            
+            for(int j = 0; j < visitantes; j++){ //visitantes
+                //pedir datos visitante
+                System.out.println("============================================");
+                System.out.println("Visitante " + (j+1));
+                System.out.println("============================================");
+                System.out.println("Ingrese su número de identificación");
+                int numIdentificacion = sc.nextInt();
+                System.out.println("Ingrese su altura en cm");
+                int alturaVisitante = sc.nextInt();
+                System.out.println("Ingrese su edad");
+                int edadVisitante = sc.nextInt();
+
+                //verificar restricciones
+                boolean permitido = true;
+                if(alturaVisitante < alturaMinAtraccion){
+                    permitido = false;
+                }
+                if((acrofobiaAtraccion == 3) && (edadVisitante < 18 || edadVisitante > 40)){
+                    permitido = false;
+                }
+                
+                //imprimir visitante
+                if(permitido){
+                    System.out.println("---------------------------------------------------------------");
+                    System.out.println("# identificación " + numIdentificacion);
+                    System.out.println("Bienvenido al Parque del Cafe, atracción " + nombreAtraccion);
+                    System.out.println("ticket #" + (j+1));
+                    System.out.println("---------------------------------------------------------------");
+                    
+                    tickets++; //aumentar contador de tickets entregados
+                } else{
+                    System.out.println("---------------------------------------------------------------");
+                    System.out.println("Estimado cliente, usted no cumple con los requisitos para esta atracción");
+                    System.out.println("---------------------------------------------------------------");
+                    
+                    restricciones++; //aumentar contador de restricciones
+                }
+            }
+            sc.nextLine();
+        }
+        
+        System.out.println("");
+        System.out.println("=================================================================================");
+        System.out.println("Fin jornada laboral");
+        System.out.println("=================================================================================");
+        System.out.println("Tickets entregados: " + tickets);
+        System.out.println("Personas restringidas para el ingreso a las atracciones " + restricciones);
+    }
+    
+}
